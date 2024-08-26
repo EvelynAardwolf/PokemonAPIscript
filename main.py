3069e0ddfd5e593f47606c03694e68e98d3a18d6
Evelyn <evelynaardwolf@gmail.com>
2024-08-26 16:22:07 +0200

    string concat fix

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ else:
 #list for data to be written to
 formattedPokemonDataList = []
 for i in pokemonGenRange:
-    print("fetching:" + str(i) + " of " + pokemonGenRange.stop)
+    print("fetching:" + str(i) + " of " + str(pokemonGenRange.stop))
     data = getPokemon(i)
     formattedPokemonData = formatPokeData(data)
     formattedPokemonDataList.append(formattedPokemonData)
