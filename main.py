1c32952a93960ec8188d78b4044ef7de2a79f327
Evelyn <evelynaardwolf@gmail.com>
2024-08-26 11:25:23 +0200

    added basic format and writecsv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import csv
 
 #function to get a pokemon from pokeapi
 def getPokemon(pokemonID: int) -> dict:
@@ -16,13 +18,64 @@ def getPokemon(pokemonID: int) -> dict:
         "stats": data.pop("stats"),
         "types": data.pop("types")
         }
-        
-        print(pokemonData)
-        return 
+        del(data)
+        #print(pokemonData)
+        return pokemonData
     else:
         print("request returned: " + response.status_code)
-        return None
+        exit(1)
+    
+
+#format the pokemon data into a readable string
+def formatPokeData(pokemonData):
+    #print(len(pokemonData['types']))
+    if len(pokemonData['types']) == 1:
+        print(f"id: {pokemonData['id']}",
+            f"Name: {pokemonData['name']}",
+            f"Weight: {pokemonData['weight']/1000} kg",
+            f"Height: {pokemonData['height']/10} m",
+            f"type 1: {pokemonData['types'][0]['type']['name']}",
+            f"type 2: none", 
+            f"base stats: ",
+            f"  hp: {pokemonData['stats'][0]['base_stat']}",
+            f"  attack: {pokemonData['stats'][1]['base_stat']}",
+            f"  defense: {pokemonData['stats'][2]['base_stat']}",
+            f"  special-attack: {pokemonData['stats'][3]['base_stat']}",
+            f"  special-defence: {pokemonData['stats'][4]['base_stat']}",
+            f"  speed: {pokemonData['stats'][5]['base_stat']}",
+            sep="\n"
+            )
+    else:
+        print(f"id: {pokemonData['id']}",
+            f"Name: {pokemonData['name']}",
+            f"Weight: {pokemonData['weight']/1000} kg",
+            f"Height: {pokemonData['height']/10} m",
+            f"type 1: {pokemonData['types'][0]['type']['name']}",
+            f"type 2: {pokemonData['types'][1]['type']['name']}", 
+            f"base stats: ",
+            f"  hp: {pokemonData['stats'][0]['base_stat']}",
+            f"  attack: {pokemonData['stats'][1]['base_stat']}",
+            f"  defense: {pokemonData['stats'][2]['base_stat']}",
+            f"  special-attack: {pokemonData['stats'][3]['base_stat']}",
+            f"  special-defence: {pokemonData['stats'][4]['base_stat']}",
+            f"  speed: {pokemonData['stats'][5]['base_stat']}",
+            sep="\n"
+            )
+    return
+
+
+def writeCSV(formattedPokemonDataList, generation):
+    with open(f'pokemonGen{generation}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ["Number", "Name", "Weight", "Height", "Type 1" "Type 2", "Stats"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(formattedPokemonDataList)
+
+
+#pokemongen = input("select generation \n")
 
 
 
-print(getPokemon(input("Enter pokemon ID \n")))
+data = getPokemon(input("Enter pokemon ID \n"))
+formatPokeData(data)
