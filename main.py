1b505f25ae96e70a444b6932ea7d2ff0b87ef79d
Evelyn <evelynaardwolf@gmail.com>
2024-08-26 16:14:45 +0200

    rewrite of formatPokeData

--- main.py
+++ main.py
@@ -33,36 +33,35 @@ def getPokemon(pokemonID: int) -> dict:
 #format the pokemon data into a dict format for .csv
 def formatPokeData(pokemonData) -> dict:
     #print(len(pokemonData['types']))
-    basestats = {
-                "hp": pokemonData['stats'][0]['base_stat'],
-                "attack": pokemonData['stats'][1]['base_stat'], 
-                "defense": pokemonData['stats'][2]['base_stat'], 
-                "special-attack": pokemonData['stats'][3]['base_stat'], 
-                "special-defence": pokemonData['stats'][4]['base_stat'], 
-                "speed": pokemonData['stats'][5]['base_stat']
+    formatted = {
+                "id": pokemonData['id'],
+                "Name": pokemonData['name'],
+                "Weight": pokemonData['weight']/1000, #divide by 1000 to get Kg
+                "Height": pokemonData['height']/10, #divide by 10 to get meters
                 }
 
     #format depending on type 2
     if len(pokemonData['types']) == 1:
-        formatted = {
-                    "id": pokemonData['id'],
-                    "Name": pokemonData['name'],
-                    "Weight": pokemonData['weight']/1000, #divide by 1000 to get Kg
-                    "Height": pokemonData['height']/10, #divide by 10 to get meters
+        formatted.update({
                     "Type 1": pokemonData['types'][0]['type']['name'],
                     "Type 2": "None",
-                    "Stats": basestats
-                    }
+                    })
     else:
-        formatted = {
-                    "id": pokemonData['id'],
-                    "Name": pokemonData['name'],
-                    "Weight": pokemonData['weight']/1000, #divide by 1000 to get Kg
-                    "Height": pokemonData['height']/10, #divide by 10 to get meters
+        formatted.update({
                     "Type 1": pokemonData['types'][0]['type']['name'],
                     "Type 2": pokemonData['types'][1]['type']['name'],
-                    "Stats": basestats
-                    }
+                    })
+        #add stats
+        formatted.update({
+            "hp": pokemonData['stats'][0]['base_stat'],
+            "attack": pokemonData['stats'][1]['base_stat'], 
+            "defense": pokemonData['stats'][2]['base_stat'], 
+            "special-attack": pokemonData['stats'][3]['base_stat'], 
+            "special-defence": pokemonData['stats'][4]['base_stat'], 
+            "speed": pokemonData['stats'][5]['base_stat']
+        })
+
+    formatted
 
     return formatted
 
